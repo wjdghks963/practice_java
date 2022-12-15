d16a61dddf3b604c0de569b8460dbb562a4522c6
wjdghks963 <chsw000@naver.com>
2022-12-15 17:45:38 +0900

    JAVA multiThread

---
+++ src/chap08/UseThread.java
@@ -0,0 +1,62 @@
+package chap08;
+
+import java.awt.*;
+
+
+/**
+ * main 스레드의 반복문 띵과 task, beepTask의 run에 들어있는 함수가 동시에 작동한다.
+ * 스레드 객체를 만들어서 Runnbale 객체를 줬기 때문에 스레드가 작동할 수 있고
+ * 메인 스레드와 함께 다른 스레드들이 멀티 스레드로 작동하기 때문에 동시 작동이 가능하다.
+ * */
+
+public class UseThread {
+    // main Thread
+    public static void main(String[] args) {
+        Runnable task = new Task();
+        Runnable beepTask = new BeepTask();
+        // 작업 스레드 객체 생성
+        Thread thread = new Thread(task);
+        Thread beepThread = new Thread(beepTask);
+
+        // 스레드 클래스의 정적 메소드로 현재 스레드 참조
+        Thread mainThread = Thread.currentThread();
+        System.out.println("프로그램 메인 스레드"+mainThread.getName());
+
+        // runable의 run 실행
+        thread.start();
+        beepThread.start();
+
+        for(int i=0;i<5;i++){
+            System.out.println("띵");
+            try{Thread.sleep(500);}catch (Exception e){}
+        }
+
+        // 스레드 이름 설정, 가져오기
+        thread.setName("sout하는 스레드");
+        beepThread.setName("beep 소리 내는 스레드");
+        System.out.println(thread.getName());
+        System.out.println(beepThread.getName());
+    }
+}
+// 작업 스레드가 실행할 수 있는 코드를 가지고 있는 객체 Runnable interface를 이용해 구현 객체를 만듦
+class Task implements Runnable{
+    @Override
+    public void run() {
+        System.out.println("메인 말고 다른 작업 스레드 실행");
+    }
+}
+
+class BeepTask implements Runnable{
+    @Override
+    public void run() {
+        Toolkit toolkit =Toolkit.getDefaultToolkit();
+        for(int i=0;i<5;i++){
+            // 하드웨어 제어로 beep 소리가 남
+            toolkit.beep();
+            try{
+                // 0.5초 정지
+                Thread.sleep(500);
+            }catch (Exception e){}
+        }
+    }
+}
